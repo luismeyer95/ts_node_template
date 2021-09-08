b5a55329d1848fc26f1e12f6e78ade1e1162fc7b
luismeyer95 <luismeyer95@gmail.com>
2021-09-08 12:50:17 +0200

    readme

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,3 @@ export const me: Info = {
     name: 'Luis',
     age: 26
 };
-
-console.log('salut');
-
-[0, 3, 6, 3, 5, 7, 7, 6]
-    .map((x) => x ** 2)
-    .filter((x) => x > 30)
-    .reduce((acc, el) => acc + el, 0);
